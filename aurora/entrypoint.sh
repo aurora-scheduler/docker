8f88c85e802db02e92d56bbecbf186af20e277ca
Renan DelValle <renanidelvalle+noreply@gmail.com>
2018-08-15 17:56:54 -0700

    Adding missing entrypoint.sh

---
+++ aurora/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+AURORA_HOME=/usr/share/aurora
+LOGDB_FILE_PATH=/var/db/aurora
+
+# Initialize replicated log
+mkdir -p $LOGDB_FILE_PATH
+mesos-log initialize --path="$LOGDB_FILE_PATH"
+
+exec /usr/sbin/aurora-scheduler \
+  -allowed_container_types="${ALLOWED_CONTAINER_TYPES:-MESOS,DOCKER}" \
+  -backup_dir="${BACKUP_DIR:-/var/lib/aurora/backups}" \
+  -cluster_name="${CLUSTER_NAME:-example}" \
+  -http_port=${PORT:-8081} \
+  -native_log_quorum_size=${QUORUM_SIZE:-1} \
+  -mesos_master_address=zk://${ZK_ENDPOINTS:-127.0.0.1:2181}/mesos \
+  -serverset_path="${ZK_SERVERSET_PATH:-/aurora/scheduler}" \
+  -native_log_zk_group_path="${ZK_LOGDB_PATH:-/aurora/replicated-log}" \
+  -native_log_file_path="$LOGDB_FILE_PATH" \
+  -thermos_executor_path="${THERMOS_EXECUTOR_PATH:-$AURORA_HOME/bin/thermos_executor.pex}" \
+  -thermos_executor_resources="$THERMOS_EXECUTOR_RESOURCES" \
+  -thermos_executor_flags="$THERMOS_EXECUTOR_FLAGS" \
+  -zk_endpoints="${ZK_ENDPOINTS:-127.0.0.1:2181}" \
+  $EXTRA_SCHEDULER_ARGS
